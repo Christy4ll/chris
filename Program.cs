6a57c48abd55ed7138de81fa1c634e2f4fcbd052
unknown <@gmail.com@gmail.com>
2023-01-26 13:57:36 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }  // Chapter three 
+            //     Class work number 1
+            // // Console.WriteLine("input a number")
+                // int numOne = Convert.ToInt32(Console.ReadLine());
+                // if(numOne%2 == 0)
+                // {
+                //     Console.WriteLine(this is an even number);
+                // }
+                // else
+                // {
+                //     Console.WriteLine(this is an odd number);
+                // }
+
+                    // Number 2
+                // Console.WriteLine("input a number")
+                // int numOne = Convert.ToInt32(Console.ReadLine());
+                // bool divisible = (numOne %5 == 0 && numOne % 7 == 0);
+                // Console.WriteLine(divisible);
+
+                    // Number 3
+                //  Console.WriteLine("Enter your digit");
+                // int digit = Convert.ToInt32(Console.ReadLine());
+                // int firstDigit = (digit / 100);
+                // Console.WriteLine(firstDigit);
+                // int second = (firstDigit) % 10;
+                // Console.WriteLine(second);
+                // if(second == 7)
+                // {
+                //     Console.WriteLine("true");
+                // }
+                // else
+                // {
+                //      Console.WriteLine("false");
+                // }
+
+                    // Number 5
+                // Console.WriteLine("input the value of a");
+                // double aValue = Convert.ToDouble(Console.ReadLine());
+                // Console.WriteLine("input the value of b");
+                // double bValue = Convert.ToDouble(Console.ReadLine());
+                // Console.WriteLine("input the value of h");
+                // double hValue = Convert.ToDouble(Console.ReadLine());
+                // double areaOfTrapezoid = (0.5)*(aValue + bValue)* (hValue);
+                // Console.WriteLine("The area of the trapezoid is " + areaOfTrapezoid);
+ 
+                    // Number 6
+                // Console.WriteLine("input the value of lenght");
+                // double lenght = Convert.ToDouble(Console.ReadLine());
+                // Console.WriteLine("input the value of breadth");
+                // double breadth = Convert.ToDouble(Console.ReadLine());
+                // double areaOfRectangle = (lenght * breadth);
+                // double perimeterOfRectangle = 2 * (lenght + breadth);
+                // Console.WriteLine("the area of the rectangle is " + areaOfRectangle);
+                // Console.WriteLine("the perimeter of the rectangle is " + perimeterOfRectangle);
+
+                    // Number 7
+                // Console.WriteLine("Inputr your earth weight");
+                // double weight = Convert.ToDouble(Console.ReadLine());
+                // double weightOnMoon = 0.17 * weight;
+                // Console.WriteLine("your approximate weight on the moon is " + weightOnMoon);
+                  
+                //      Number 8
+                //  Console.WriteLine("Enter the value of x");
+                //  int xValue = Convert.ToInt32(Console.ReadLine());
+                //  Console.WriteLine("Enter the value of y");
+                //  int yValue = Convert.ToInt32(Console.ReadLine());
+                //  bool isTrue =((x * x) + (y * y) <= 5 * 5);
+                //  Console.WriteLine(isTrue);
+
+                    // Number 9
+
+                    // Number 10
+              /*  Console.WriteLine("Input four digits number");
+                int num = Convert.ToInt32(Console.ReadLine());
+                int digitOne = (num % 10);
+                int digitTwo = (num / 10) % 10;
+                int digitThree = (num / 100) % 10;
+                int digitFour = (num / 1000) % 10;
+                int theSum = (digitOne + digitTwo + digitThree + digitFour);
+                Console.WriteLine("their sum is " +theSum);
+                Console.WriteLine(digitOne);
+                Console.WriteLine(digitTwo);
+                Console.WriteLine(digitThree);
+                Console.WriteLine(digitFour);
+                Console.WriteLine(digitOne+""+digitTwo+""+digitThree+""+digitFour);
+                Console.WriteLine(digitFour+""+digitThree+""+digitTwo+""+digitOne);
+               
+                int a = 0;
+                a = digitTwo;
+                digitTwo = digitFour;
+                digitFour = a;
+                Console.WriteLine(digitOne+""+digitTwo+""+digitThree+""+digitFour);
+
+                int z = 0;
+                z = digitTwo;
+                digitTwo = digitOne;
+                digitOne = z;
+                Console.WriteLine(digitOne+""+digitTwo+""+digitThree+""+digitFour);*/
+
+                // Number 14
+          // Console.WriteLine("input your number");
+         // int numOne = Convert.ToInt32(Console.ReadLine());
+           // bool isPrime = true;
+    // for(int i = 2; i < numOne; i++)
+    // {
+    //     if(numOne % i == 0)
+    //     {
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+    // if(isPrime == true)
+    // {
+    //     Console.WriteLine("This number is prime");
+    // }
+    // else
+    // {
+        // Console.WriteLine("This number is not prime");
+    // }
+
+    
+
+
+}
